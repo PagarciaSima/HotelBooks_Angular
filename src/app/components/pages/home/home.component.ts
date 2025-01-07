2deee2769e77033c1a8e58ddcb7233f901391e2e
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-07 16:59:01 +0100

    Styling searcher

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DpDatePickerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  datePickerConfig = {
+    format: 'YYYY-MM-DD'
+  };
 }
