f1a494839b70704df633a0d32e6bdffb2758a56f
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-17 05:54:01 +0100

    Service class Angular

--- src/app/services/hotels.service.ts
+++ src/app/services/hotels.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Hotel } from '../interface/hotel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
 
-  constructor() { }
+  private apiURL = 'http://localhost:8082/api/hotels/available';
+
+  constructor(private http: HttpClient) { }
+
+  getHotels(startDate: string, endDate: string, cityId: number) {
+    return this.http.get<Hotel[]>(`${this.apiURL}?startDate=${startDate}&endDate=${endDate}&cityId=${cityId}`);
+  }
 }
