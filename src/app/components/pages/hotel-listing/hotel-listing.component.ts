34e65eeab75fbd3eae7616562ed9649e16a47499
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-17 07:50:31 +0100

    Display hotels data from service response

--- src/app/components/pages/hotel-listing/hotel-listing.component.ts
+++ src/app/components/pages/hotel-listing/hotel-listing.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hotel } from '../../../interface/hotel';
 
 @Component({
@@ -10,7 +10,12 @@ import { Hotel } from '../../../interface/hotel';
   styleUrl: './hotel-listing.component.scss'
 })
 export class HotelListingComponent {
+
   prices = [100, 200, 300, 400, 500]; 
 
   @Input() hotels: Hotel[] = [];
+
+  trackByID(index: number, hotel: Hotel): any {
+    return hotel.id; // Devuelve el cityID como clave única
+  }
 }
