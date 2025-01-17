c957c2ce17834778ba1057260e0fa18cb377947d
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-01-17 06:47:17 +0100

    Consuming hotel service

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { DpDatePickerModule } from 'ng2-date-picker';
 import { HotelListingComponent } from '../hotel-listing/hotel-listing.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Hotel } from '../../../interface/hotel';
+import { HotelsService } from '../../../services/hotels.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DpDatePickerModule, HotelListingComponent],
+  imports: [
+    DpDatePickerModule,
+    HotelListingComponent,
+    HttpClientModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -14,4 +21,25 @@ export class HomeComponent {
   datePickerConfig = {
     format: 'YYYY-MM-DD'
   };
+
+  hotels: Hotel[] = [];
+
+  constructor(private hotelService: HotelsService) {
+    this.loadHotels();
+  }
+
+  loadHotels(): void {
+    const startDate = '2024-10-10';
+    const endDate = '2024-10-20';
+    const cityID = 1;
+    this.hotelService.getHotels(startDate, endDate, cityID).subscribe({
+      next: (data) => {
+        this.hotels = data;
+        console.log('hotels: ', this.hotels);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    })
+  }
 }
